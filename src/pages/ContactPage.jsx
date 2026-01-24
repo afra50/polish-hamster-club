10cd3f66242bb0320914c4e2da41a22ae246c9de
Chris <chrisoo1991@gmail.com>
2026-01-24 18:50:59 +0100

    media queries podstrony kontakt

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -6,49 +6,51 @@ import { MapPin } from "lucide-react";
 export default function ContactPage() {
 	return (
 		<main className="page-contact">
-			<header className="header-contact">
-				<div className="hero-contact">Jesteśmy tu dla Ciebie</div>
-				<h1 className="contact-heading">Skontaktuj się z nami</h1>
-				<p>
-					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio
-					recusandae doloribus molestias beatae eius rem sed ducimus eaque
-					natus. Aliquam reiciendis nemo reprehenderit quos cumque dolore
-					aliquid tempora aspernatur minima.
-				</p>
-			</header>
-			<section className="contact-fonts">
-				<div className="contact-container">
-					<div className="contact-row">
-						<div className="contact-icon">
-							<Mail />
+			<div className="page-contact-inner">
+				<header className="header-contact">
+					<div className="hero-contact">Jesteśmy tu dla Ciebie</div>
+					<h1 className="contact-heading">Skontaktuj się z nami</h1>
+					<p>
+						Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio
+						recusandae doloribus molestias beatae eius rem sed ducimus eaque
+						natus. Aliquam reiciendis nemo reprehenderit quos cumque dolore
+						aliquid tempora aspernatur minima.
+					</p>
+				</header>
+				<section className="contact-fonts">
+					<div className="contact-container">
+						<div className="contact-row">
+							<div className="contact-icon">
+								<Mail />
+							</div>
+							<div className="contact-text">
+								<div className="contact-label">e-mail</div>
+								<div clasNames="contact-value">chrum@kwik.pl</div>
+							</div>
 						</div>
-						<div className="contact-text">
-							<div className="contact-label">e-mail</div>
-							<div clasNames="contact-value">chrum@kwik.pl</div>
-						</div>
-					</div>
 
-					<div className="contact-row">
-						<div className="contact-icon">
-							<Phone />
+						<div className="contact-row">
+							<div className="contact-icon">
+								<Phone />
+							</div>
+							<div className="contact-text">
+								<div className="contact-label">Telefon</div>
+								<div className="contact-value">+48 123 456 789</div>
+							</div>
 						</div>
-						<div className="contact-text">
-							<div className="contact-label">Telefon</div>
-							<div className="contact-value">+48 123 456 789</div>
-						</div>
-					</div>
 
-					<div className="contact-row">
-						<div className="contact-icon">
-							<MapPin />
-						</div>
-						<div className="contact-text">
-							<div className="contact-label">Miejscowsość</div>
-							<div className="contact-value">Chrumianki, Polska</div>
+						<div className="contact-row">
+							<div className="contact-icon">
+								<MapPin />
+							</div>
+							<div className="contact-text">
+								<div className="contact-label">Miejscowsość</div>
+								<div className="contact-value">Chrumianki, Polska</div>
+							</div>
 						</div>
 					</div>
-				</div>
-			</section>
+				</section>
+			</div>
 		</main>
 	);
 }
