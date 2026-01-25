b3675de0caf09d2d2fa8de1c7710abb415a2bc77
Chris <chrisoo1991@gmail.com>
2026-01-25 20:20:18 +0100

    strona w budowie -pierwsza faza

--- src/pages/BuildPage.jsx
+++ src/pages/BuildPage.jsx
@@ -1,13 +1,32 @@
 import React from "react";
-import { Home } from "lucide-react";
 import Button from "../components/ui/Button";
 import buildHamster from "../assets/build-hamster.png";
 import "../styles/pages/buildPage.scss";
 
 export default function BuildPage() {
-  return (
-    <main className="build-page">
-      <p>w budowie</p>
-    </main>
-  );
+	return (
+		<main className="buildpage">
+			<div className="buildpage-inner">
+				<div className="buildpage-png">
+					<img
+						src={buildHamster}
+						alt="Śmieszny Chomik architekt w ochronnym kasku i projektem budowy"
+					/>
+				</div>
+				<header className="buildpage-header">
+					<h1 className="buildpage-h">Strona w budowie</h1>
+					<p>
+						Właśnie rozbudowujemy naszą norkę. Nasi mali architekci pracują nad
+						tą sekcją, aby dostarczyć Ci jeszcze więcej wiedzy i
+						funkcjonalności.
+					</p>
+					<strong>Zajrzyj do nas ponownie wkrótce.</strong>
+				</header>
+
+				<Button to="/" variant="primary">
+					Wróć na stronę główną
+				</Button>
+			</div>
+		</main>
+	);
 }
